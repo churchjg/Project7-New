510955394da3ec76f3618edbb061dae3a5cb4722
Jon <churchjg283@gmail.com>
2020-11-09 10:47:39 -0500

    API coming in

--- front-end-crud/src/App.js
+++ front-end-crud/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Container, Row, Col } from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+let url = "https://getcookingwithjon.herokuapp.com/"
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: ""
+      , name: ''
+      , image: ''
+      , category: ''
+      , tags: ''
+      , instructions: ''
+      , area: ''
+      , video: ''
+      , ready: false
+    }
+  }
+  componentDidMount() {
+    console.log("hey we are in Homepage")
+    fetch(`${url}`)
+      .then(res => res.json())
+      .then(res => {
+        this.data = res
+        console.log(res)
+        this.setState({
+          ready: true
+        })
+      })
+  }
+
+  renderHomepage = () => {
+    if (this.state.ready === true) {
+      return (
+        <Route path="/"
+          render={() => <App meal={this.data} />}
+          exact
+        />
+      )
+    } else { return <h1>Page Loading....</h1> }
+  }
+  render() {
+    return (
+        <main>
+            {this.renderHomepage()}
+            <Route exact path={`/${this.state.id}`}
+                render={(routerProps) => <App info={this.state}
+                    {...routerProps}
+                />}
+            />
+        </main>
+    )
+}
 }
 
 export default App;
